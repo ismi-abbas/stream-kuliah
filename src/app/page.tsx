6746a493fb143115d86920b82808e151c26336b0
Abbas <muhdabbas98@gmail.com>
2024-07-17 13:04:58 +0800

    initial commit

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,95 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import {
+  Button,
+  Card,
+  CardBody,
+  CardHeader,
+  Divider,
+  Flex,
+  Heading,
+  Image,
+  Link,
+  Text,
+} from "@chakra-ui/react";
+
+export interface Kuliah {
+  id: string;
+  title: string;
+  description: string;
+  thumbnail: string;
+  videoUrl: string;
+  ustaz: string;
+}
+const kuliahList: Kuliah[] = [
+  {
+    id: "1",
+    title: "Fathul Muin",
+    description: "Bab Nikah",
+    ustaz: "Ustaz Mahadi",
+    thumbnail: "",
+    videoUrl: "",
+  },
+  {
+    id: "2",
+    title: "Fathul Muin",
+    description: "Bab Nikah",
+    ustaz: "Ustaz Mahadi",
+    thumbnail: "",
+    videoUrl: "",
+  },
+];
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>src/app/page.tsx</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <Flex direction="column" paddingX={20} marginY={10}>
+      <Heading display="flex" alignItems="center" gap={2}>
+        Stream Kuliah
+        <span>
+          <Image src="/masjid.svg" alt="Logo" boxSize="40px" display="inline" />
+        </span>
+      </Heading>
+      <Text fontSize="md">Kuliah kitab di hujung jari</Text>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+      <Text fontSize="sm" marginTop={2}>
+        Laman Web ini menyediakan kuliah kitab-kitab agama yang diambil dari
+        pelbagai Ustaz yang bertauliah mengajar kitab-kitab turath. Rujuk{" "}
+        <Link href="/senarai-ustaz">pautan ini</Link> untuk senarai Ustaz yang
+        terlibat.
+      </Text>
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      <Flex alignItems="start" gap={4} mt={4} direction="column">
+        <Flex gap={2}>
+          <Button colorScheme="teal">Senarai Kuliah</Button>
+          <Button colorScheme="teal">Senarai Ustaz</Button>
+        </Flex>
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        <Flex direction="column">
+          <Card>
+            <CardHeader>Ustaz Mahadi</CardHeader>
+            <CardBody>
+              <Image src="" />
+            </CardBody>
+          </Card>
+        </Flex>
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore starter templates for Next.js.</p>
-        </a>
+        <Heading fontSize="large" mt={4}>
+          Serarai Kuliah
+        </Heading>
 
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+        <Divider my={2} />
+
+        <Flex direction="row" gap={4}>
+          {kuliahList.map((kuliah) => (
+            <Card key={kuliah.id} w={200} h={250}>
+              <CardHeader fontWeight={600}>{kuliah.title}</CardHeader>
+              <CardBody>
+                <Image src={kuliah.thumbnail} />
+                <Text>{kuliah.description}</Text>
+                <Text>{kuliah.ustaz}</Text>
+              </CardBody>
+            </Card>
+          ))}
+        </Flex>
+      </Flex>
+    </Flex>
   );
 }
---
+++ src/app/senarai-ustaz/page.tsx
@@ -0,0 +1,10 @@
+import { Flex } from "@chakra-ui/react";
+import React from "react";
+
+export default function SenaraiUstaz() {
+  return (
+    <Flex>
+      <h1>Senarai Ustaz</h1>
+    </Flex>
+  );
+}
